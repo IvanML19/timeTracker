be7aa3003f890649b4af72e30b591182afdfd216
IvanML19 <ivanmartinlopez19@hotmail.com>
2020-04-23 18:41:17 +0200

    creating table from init.sql

--- etc/infrastructure/database/init.sql
+++ etc/infrastructure/database/init.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE IF NOT EXISTS degustabox;
+
+CREATE TABLE IF NOT EXISTS `degustabox`.`timetracker` (
+  `uuid` VARCHAR(37) NOT NULL,
+  `name` VARCHAR(40) NULL,
+  `time` INT NULL,
+  `dateadd` DATETIME NOT NULL,
+  `dateupd` DATETIME NULL,
+  PRIMARY KEY (`uuid`, `dateadd`));
